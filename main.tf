cadf5a97f668f2a368acfca02b8747b32eb7eaed
arunawsdevops <arunawscloud53@gmail.com>
2024-06-20 19:27:47 +0530

    adding terraform files

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "k8-cluster" {
   name     = "k8-cluster"
-  role_arn = "arn:aws:iam::338665245618:role/eks-cluster-role"
+  role_arn = "arn:aws:iam::670004487191:role/eks-cluster-role"
   version  = "1.28"
 
   vpc_config {
@@ -12,7 +12,7 @@ resource "aws_eks_cluster" "k8-cluster" {
 resource "aws_eks_node_group" "k8-cluster-node-group" {
   cluster_name    = aws_eks_cluster.k8-cluster.name
   node_group_name = "k8-cluster-node-group"
-  node_role_arn   = "arn:aws:iam::338665245618:role/node-role"
+  node_role_arn   = "arn:aws:iam::670004487191:instance-profile/node-role"
   subnet_ids      = ["subnet-0c542ada8b39ab9cc", "subnet-0b686a2f50c5f6be2"]
 
   scaling_config {

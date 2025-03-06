7848cada3ebe4e3d3cb69496bc80e832961f3b50
remyars18 <157706585+remyars18@users.noreply.github.com>
2025-03-06 23:00:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks-cluster-terra-state"
+    bucket = "terra-state-new-infra-remya-123"
     key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
